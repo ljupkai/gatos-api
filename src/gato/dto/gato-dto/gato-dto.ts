495503a0b8d845e44bab8f2b7402f6530a0ab8a9
ljupka <ljupka.ivanova@gmail.com>
2023-04-16 18:30:42 +0200

    ValidationPipes habilitado globalmente, decoradores añadidos en GatoDto

--- src/gato/dto/gato-dto/gato-dto.ts
+++ src/gato/dto/gato-dto/gato-dto.ts
@@ -1,8 +1,17 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class GatoDto {
+  @IsString()
+  @IsNotEmpty()
   readonly nombre: string;
+  @IsNotEmpty()
   readonly edad: number;
   readonly castrado: boolean;
+  @IsString()
+  @IsNotEmpty()
   readonly color: string;
+  @IsNotEmpty()
+  @IsString()
   readonly descripcion: string;
   readonly reservado: boolean;
   readonly personalidad: string[];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Gatos')
